9287759f924e154f1fedcbedbbcc708b7d01fc20
Efe Gücüyeter <57722420+efegucuyeter@users.noreply.github.com>
2024-12-06 16:56:24 +0300

    themes dosyasını güncelledim

---
+++ lib/core/themes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Colors.orange; // Birincil renk
+  static const Color secondary = Colors.white; // İkincil renk (ör: arka plan)
+  static const Color accent = Colors.grey; // Yardımcı renk (ör: kenarlıklar)
+  static const Color textPrimary = Colors.black; // Ana metin rengi
+  static const Color textSecondary = Colors.orange; // Yardımcı metin rengi
+}
